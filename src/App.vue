3d21dbcf69a529637b997b9b738ad2271256cf22
Nguyen Gia Huy <huy20041512@gmail.com>
2025-03-04 20:36:33 +0700

    fix

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Header from './components/HeaderComponent.vue';
+</script>
+
+<template>
+  <div class="min-h-screen flex flex-col bg-gray-100">
+    <Header></Header>
+    <main class="flex-1 container mx-auto px-6 py-4">
+      <RouterView />
+    </main>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,21 @@
+<template>
+<header class="bg-white shadow-md">
+    <div class="container mx-auto px-6 py-4 flex justify-between items-center">
+      <RouterLink to="/">
+        <div class = " flex items-center">
+          <img class = "h-10 w-auto mx-4" src="./assets/logo.png" alt="">
+          <div class="text-xl font-bold text-gray-800">
+          BookZen
+        </div>
+        </div>
+        
+        
+      </RouterLink>
+
+      <nav class="space-x-6">
+        <RouterLink to="/" class="text-gray-600 hover:text-blue-600">Trang chủ</RouterLink>
+        <RouterLink to="/login" class="text-gray-600 hover:text-blue-600">Đăng nhập</RouterLink>
+      </nav>
+    </div>
+</header>
+</template>
